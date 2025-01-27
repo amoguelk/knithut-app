e4a92cc8f49cf65ce42794481b027cda11d3ecf9
Andrea Moguel K. <31602026+amoguelk@users.noreply.github.com>
2025-01-27 12:42:35 -0600

    Changed setting screen style

--- src/screens/SettingsScreen/index.jsx
+++ src/screens/SettingsScreen/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
+import { useTheme } from 'contexts/ThemeContext';
 import ThemeButton from './ThemeButton';
 import LanguageButton from './LanguageButton';
+import getStyles from './styles';
 
-const SettingsScreen = () => (
-  <View>
-    <ThemeButton />
-    <LanguageButton />
-  </View>
-);
+const SettingsScreen = () => {
+  const {
+    theme: { colors },
+  } = useTheme();
+  const styles = getStyles(colors);
+
+  return (
+    <View style={styles.container}>
+      <ThemeButton />
+      <LanguageButton />
+    </View>
+  );
+};
 
 export default SettingsScreen;
